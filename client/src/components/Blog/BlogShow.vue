603c9a42ec52ba89234429f0a10ba8e2bb034bfe
Rasa <abdurasul2004.02@gmail.com>
2023-05-15 17:01:43 +0500

    blogshow component images path fixed

--- client/src/components/Blog/BlogShow.vue
+++ client/src/components/Blog/BlogShow.vue
@@ -136,7 +136,7 @@
             </div><!--/price-range-->
 
             <div class="shipping text-center"><!--shipping-->
-              <img src="{{ asset('images/home/shipping.jpg') }}" alt="" />
+              <img src="@/assets/images/home/shipping.jpg" alt="" />
             </div><!--/shipping-->
           </div>
         </div>
@@ -160,7 +160,7 @@
 								</span>
               </div>
               <a href="">
-                <img src="{{ asset('images/blog/blog-one.jpg') }}" alt="">
+                <img src="@/assets/images/blog/blog-one.jpg" alt="">
               </a>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p> <br>
@@ -204,12 +204,12 @@
           </div><!--/rating-area-->
 
           <div class="socials-share">
-            <a href=""><img src="{{ asset('images/blog/socials.png') }}" alt=""></a>
+            <a href=""><img src="@/assets/images/blog/socials.png" alt=""></a>
           </div><!--/socials-share-->
 
           <div class="media commnets">
             <a class="pull-left" href="#">
-              <img class="media-object" src="{{ asset('images/blog/man-one.jpg') }}" alt="">
+              <img class="media-object" src="@/assets/images/blog/man-one.jpg" alt="">
             </a>
             <div class="media-body">
               <h4 class="media-heading">Annie Davis</h4>
@@ -231,7 +231,7 @@
               <li class="media">
 
                 <a class="pull-left" href="#">
-                  <img class="media-object" src="{{ asset('images/blog/man-two.jpg') }}" alt="">
+                  <img class="media-object" src="@/assets/images/blog/man-two.jpg" alt="">
                 </a>
                 <div class="media-body">
                   <ul class="sinlge-post-meta">
@@ -245,7 +245,7 @@
               </li>
               <li class="media second-media">
                 <a class="pull-left" href="#">
-                  <img class="media-object" src="{{ asset('images/blog/man-three.jpg') }}" alt="">
+                  <img class="media-object" src="@/assets/images/blog/man-three.jpg" alt="">
                 </a>
                 <div class="media-body">
                   <ul class="sinlge-post-meta">
@@ -259,7 +259,7 @@
               </li>
               <li class="media">
                 <a class="pull-left" href="#">
-                  <img class="media-object" src="{{ asset('images/blog/man-four.jpg') }}" alt="">
+                  <img class="media-object" src="@/assets/images/blog/man-four.jpg" alt="">
                 </a>
                 <div class="media-body">
                   <ul class="sinlge-post-meta">
